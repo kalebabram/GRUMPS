5a2cc2e1afd05fd5f739c288f161ceb7d07ecace
kalebabram <abram.kaleb@gmail.com>
2024-09-03 18:54:22 -0400

    Update __init__.py

--- src/grumps/api/__init__.py
+++ src/grumps/api/__init__.py
@@ -2,12 +2,16 @@ from grumps.__about__ import __author__, __version__
 from grumps.core.grumpsObj import grumpsObj
 from grumps.modes.modes import regularMode, targetMode, sigmaMode, smallMode, cliqueMode, summaryMode, targetMode, removerMode
 from grumps.api.pipeLine import pipeLine
+from grumps.core.distmat_converter import outputConverter, distmatConverter
 
 __all__ = [
-	# objects
+	# properties
 	"__author__",
 	"__version__",
+	# objects
+	"outputConverter",
 	"grumpsObj",
+	# functions
 	"regularMode",
 	"targetMode",
 	"summaryMode",
@@ -16,4 +20,5 @@ __all__ = [
 	"removerMode",
 	"cliqueMode",
 	"pipeLine",
+	"distmatConverter",
 ]
